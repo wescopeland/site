aaeda721e50ad56de178135054745959c9aa3a8c
Wes Copeland <wlcopeland1@gmail.com>
2022-04-27 19:38:54 -0400

    chore(deps): bump jest to latest

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,6 @@ const createJestConfig = nextJest({
 const customJestConfig = {
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   moduleDirectories: ["node_modules", "<rootDir>/"],
-  testEnvironment: "jest-environment-jsdom",
   moduleNameMapper: {
     "^@/(.*$)": "<rootDir>/src/$1"
   }
